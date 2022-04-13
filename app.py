37543a1d8101c12f68ad8970bb5bd2d9c160feac
meadhu <978096002@qq.com>
2022-04-13 22:45:19 +0800

    logs

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from __future__ import absolute_import
 
+import logging
 import os
 import sys
 
@@ -11,16 +12,32 @@ import tornado.ioloop
 
 from tornado.options import define, options
 
-from common.GenTool import GenTool
+from tornado.log import enable_pretty_logging
+enable_pretty_logging()
 
 define("port", default=3001, help="run on the given port", type=int)
 define("env", default='dev', type=str)
 os.environ['app_env'] = options.env
 
+# 这里配置的是日志的路径，配置好后控制台的相应信息就会保存到目标路径中。
+if not os.path.exists("logs"):
+    os.mkdir("logs")
+options.log_file_prefix = os.path.join(os.path.dirname(__file__), 'logs/tornado_main.log')
+
 # print(os.environ)
 
 from config import *
 from handler import *
+
+# 格式化日志输出格式
+# 默认是这种的：[I 160807 09:27:17 web:1971] 200 GET / (::1) 7.00ms
+# 格式化成这种的：[2016-08-07 09:38:01 执行文件名:执行函数名:执行行数 日志等级] 内容消息
+class LogFormatter(tornado.log.LogFormatter):
+    def __init__(self):
+        super().__init__(
+            fmt='%(color)s[%(asctime)s %(filename)s:%(funcName)s:%(lineno)d %(levelname)s]%(end_color)s %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
 
 
 class Application(tornado.web.Application):
@@ -57,7 +74,8 @@ def main():
         (r"/admin/task/([^/]+)", TaskHandler),  # 任务管理
         (r"/admin/user/([^/]+)", UserHandler),  # 用户管理
     ]
-    tornado.options.parse_command_line()
+    #tornado.options.parse_command_line()
+    [i.setFormatter(LogFormatter()) for i in logging.getLogger().handlers]
     # http_server = tornado.httpserver.HTTPServer(Application(router, default_handler_class=CustomExceptionHandler, debug=(options.env == 'dev')))
     http_server = tornado.httpserver.HTTPServer(Application(router, debug=(options.env == 'dev')))
     http_server.listen(options.port)
@@ -68,15 +86,18 @@ if __name__ == '__main__':
     # 通过表名, 反向生成Model
     # python app.py gen_model <表名>
     if 'gen_model' in sys.argv:
+        from common.GenTool import GenTool
         gen = GenTool()
         table_name = sys.argv[-1]
         gen.gen_model(table_name=table_name)
     elif 'gen_crud' in sys.argv:
+        from common.GenTool import GenTool
         # 通过Model名称，生成CRUD页面和功能
         # python app.py gen_crud <Model名称>
         gen = GenTool()
         table_name = sys.argv[-1]
         gen.gen_crud(table_name=table_name)
     else:
+        tornado.options.parse_command_line()
         print("项目已启动, 访问地址: http://localhost:%s" % options.port)
         main()
